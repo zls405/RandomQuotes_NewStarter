7aafa444c9239a89d4ff34ac092ebb7a6ef9fc69
Mark Harrison <mark.harrison@octopus.com>
2022-05-06 19:14:10 +0100

    Update get-started-worker.ps1

--- get-started-worker.ps1
+++ get-started-worker.ps1
@@ -2,5 +2,5 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install dotnet-5.0-sdk -y
-choco install dotnet-5.0-runtime -y
+choco install dotnet-6.0-sdk -y
+choco install dotnet-6.0-runtime -y
