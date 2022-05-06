625e0ff66bd892c01cd8d4704f5185783f7812dd
Mark Harrison <mark.harrison@octopus.com>
2022-05-06 19:14:39 +0100

    Update get-started-sql-server.ps1

--- get-started-sql-server.ps1
+++ get-started-sql-server.ps1
@@ -1,8 +1,8 @@
 # Run me on the server hosting your SQL Server
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install dotnet-5.0-sdk -y
-choco install dotnet-5.0-runtime -y
+choco install dotnet-6.0-sdk -y
+choco install dotnet-6.0-runtime -y
 
 choco install sql-server-express -y
 
